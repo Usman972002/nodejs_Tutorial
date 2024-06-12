724287a3b83bff9fead6b1f333231df6dfaca6c4
Usman972002 <usmanfaruq21@gmail.com>
2024-06-12 09:35:55 +0530

    added console

--- routes/person.js
+++ routes/person.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const person = require('../models/Person');
-const {jwtMiddleWare,generateToken} = require('../jwt');
+const { jwtMiddleWare, generateToken } = require('../jwt');
 
 router.post('/signup', async (req, res) => {
     try {
@@ -11,7 +11,7 @@ router.post('/signup', async (req, res) => {
         const response = await newPerson.save();
         console.log('New Person Saved');
         const token = generateToken(response.email);
-        res.status(200).json({"response":response,"token":token});
+        res.status(200).json({ "response": response, "token": token });
     }
     catch (error) {
         console.log(error);
@@ -19,50 +19,52 @@ router.post('/signup', async (req, res) => {
     }
 })
 
-router.post('/login',async(req,res)=>{
-    try{
-        const {username,password} = req.body;
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
 
-        const user = await person.findOne({username:username});
-        if(!user){
-            res.status(401).json({message:"Invalid UserName"});
-        }else{
+        const user = await person.findOne({ username: username });
+        if (!user) {
+            res.status(401).json({ message: "Invalid UserName" });
+        } else {
             const passwordMatch = await user.comparePassword(password);
-            if(!passwordMatch){
-                res.status(401).json({message:"Invalid Password"});
-            }else{
+            if (!passwordMatch) {
+                res.status(401).json({ message: "Invalid Password" });
+            } else {
                 const token = generateToken(user.email);
                 res.status(200).json(token);
             }
         }
-    }catch(err){
-        res.status(501).json({message :"Internal Server Error"});
+    } catch (err) {
+        console.log(err);
+        res.status(501).json({ message: "Internal Server Error" });
     }
 })
 
 // Get Request 
-router.get('/',jwtMiddleWare, async(req,res)=>{
-    try{
+router.get('/', jwtMiddleWare, async (req, res) => {
+    try {
         const response = await person.find();
         res.status(200).json(response);
-    }catch(err){
-        res.status(500).json({message:"Internal Server Error"})
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Internal Server Error" })
     }
 })
 
 // Parametarized API 
-router.get('/:workType', async(req,res)=>{
-    try{
+router.get('/:workType', async (req, res) => {
+    try {
         const workType = req.params.workType;
-        if(workType === 'chef' || workType === 'waiter' || workType === 'manager'){
-            const response = await person.find({work:workType});
+        if (workType === 'chef' || workType === 'waiter' || workType === 'manager') {
+            const response = await person.find({ work: workType });
             res.status(200).json(response);
-        }else{
-            res.status(404).json({error:"Invalid Work Type"});
+        } else {
+            res.status(404).json({ error: "Invalid Work Type" });
         }
 
-    }catch(err){
-        res.status(500).json({message:"Internal Server Error"})
+    } catch (err) {
+        res.status(500).json({ message: "Internal Server Error" })
     }
 })
 
